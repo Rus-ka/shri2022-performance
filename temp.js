4ae6b979ab523d48616dc943c8a3b48bf64a93de
Ruslan Kaziev <rrkaziev@gmail.com>
2022-11-05 22:47:27 +0300

    removed unused js

--- temp.js
+++
@@ -1,9 +0,0 @@
-function svgFileToString(iconpath){
-    fetch(iconpath)
-      .then(response => response.text())
-      .then(text => {
-        console.log(text);
-        // do whatever
-      });
-  }
-  svgFileToString(assets/icon_temperature.svg)
